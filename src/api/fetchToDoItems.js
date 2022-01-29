063add02d6dad013eaa47cfb30ea368d98960cd8
mateusz7812 <mateusz78122@gmail.com>
2022-01-29 13:36:45 +0100

    delete unused files

--- src/api/fetchToDoItems.js
+++
@@ -1,8 +0,0 @@
-
-export const fetchTodoItems = () => {
-    //Asynchronous api call
-    return new Promise((resolve) => {
-        setTimeout(resolve([]), 200);
-    });
-}
-
--- src/api/updateToDoItem.js
+++
@@ -1,8 +0,0 @@
-
-export const updateToDoItem = (item) => {
-    //Asynchronous api call
-    return new Promise((resolve) => {
-        setTimeout(resolve(item), 200);
-    });
-}
-
